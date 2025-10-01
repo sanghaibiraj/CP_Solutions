ffd08faaff9fed4277d3b80051644d8ad9427d0f
sanghaibiraj <sanghaibiraj@gmail.com>
2025-10-01 13:28:02 +0000

    cp31 sheet 1700

---
+++ Codeforces/1800-1899/1833/F_Ira_and_Flamenco.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define cout(a) cout << (a) << endl
+#define nl cout << endl
+#define fi(i,s,e) for(int i=s;i<e;i++)
+#define ain(i,arr,n); for(int i=0;i<n;i++){ cin >> arr[i];}
+#define vl vector<ll>
+#define vi vector<int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+using namespace std;
+
+const ll mod = 1e9+7;
+/*dfs */
+// void dfs(int vertex){
+//     //Take action on vertex after entering the vertex
+//     vis[vertex]=1;
+//     for(int child :g[vertex]){
+//         //Take action on child before entering the child node
+//         if(vis[child])continue;
+//         dfs(child);
+//         // Take action on child after exiting the child node
+//     }
+//     // Take action on vertex before exiting the vertex
+// }
+const int dsu_N = 1e4;
+int dsu_parent[dsu_N];
+int dsu_Size[dsu_N];
+void make(int v){
+  dsu_parent[v]=v;
+  dsu_Size[v]=1;
+}
+int find(int v){
+  if(v == dsu_parent[v]) return v;
+  return dsu_parent[v]=find(dsu_parent[v]);
+}
+void Union(int a, int b){
+  a=find(a);
+  b=find(b);
+  if(a!=b){
+    if(dsu_Size[a]<dsu_Size[b]) swap(a,b);
+    dsu_parent[b] = a;
+    dsu_Size[a]+=dsu_Size[b];
+  }
+}
+bool prime(ll n){
+  if (n < 2) return false;
+  if (n <= 3) return true;
+  if (n % 2 == 0 || n % 3 == 0) return false;
+  for (int i=5; i * i <= n; i += 6)
+    if (n % i == 0 || n % (i+2) == 0)
+      return false;
+  return true;
+}
+ll power(ll a, ll b){
+  ll res =1;
+  while(b>0){
+    if(b%2==1)
+      res = ((res%mod) * (a%mod))%mod;
+    a = ((a%mod)*(a%mod))%mod;
+    b /= 2;
+  }
+  return res%mod;
+}
+ll mod_inv(ll a) {
+    return power(a, mod - 2);
+}
+
+void solve(){
+  ll n, m;
+  cin >> n >> m;
+  vl v(n);
+  ain(i,v,n);
+  if(m==1){
+    cout(n);
+    return;
+  }
+  sort(v.rbegin(), v.rend());
+  ll c=1, len=1, tmp=1, ans=0;
+  queue<ll> q;
+  fi(i,1,n){
+    if(v[i]==v[i-1]) c++;
+    else if(v[i]==v[i-1]-1){
+        tmp = tmp * c % mod;
+        q.push(c);
+        c=1;
+        len++;
+        if(len>m){
+            tmp = tmp * mod_inv(q.front()) %mod;
+            q.pop();
+            len--;
+        }
+    }
+    else{
+        tmp = 1;
+        c=1;
+        len=1;
+        while(q.size()) q.pop();
+    }
+    if(len==m){
+        ans = (ans + tmp) %mod;\
+    }
+  }
+  cout(ans);  
+}
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t=1;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+}
